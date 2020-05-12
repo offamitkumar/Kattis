3d3e147ea60737d98cd7cec5b65919b083284914
Amit Kumar <offamitkumar@gmail.com>
2020-05-12 17:34:02 +0530

    simple observation

---
+++ Quadrant Selection/quadrant.cpp
@@ -0,0 +1,51 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int x , y; cin>>x>>y;
+    if(x<0 and y<0){
+        puts("3");
+    }else if(x<0 and y>0){
+        puts("2");
+    }else if(x>0 and y>0){
+        puts("1");
+    }else if(x>0 and y<0){
+        puts("4");
+    }
+    return 0;
+}
