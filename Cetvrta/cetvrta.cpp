d69767fb56f18be2d9368b88f0c63bed30f95584
Amit Kumar <offamitkumar@gmail.com>
2020-05-17 18:53:55 +0530

    simple

---
+++ Cetvrta/cetvrta.cpp
@@ -0,0 +1,48 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int x = 0 , y = 0;
+    for(int a,b,i=0;i<3;++i){
+        cin>>a>>b;
+        x^=a;
+        y^=b;
+    }
+    cout<<x<<' '<<y<<'\n';
+    return 0;
+}
