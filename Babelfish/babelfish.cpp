8fb51afd79dcb1661b2b8e357c6ea2d61415d8b3
Amit Kumar <offamitkumar@gmail.com>
2020-05-17 19:34:25 +0530

    map

---
+++ Babelfish/babelfish.cpp
@@ -0,0 +1,57 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    unordered_map<string,string>m;
+    string a,b;
+    while(getline(cin,a)&& a!=""){
+        stringstream ss(a);
+        ss>>a;
+        ss>>b;
+        m[b]=a;
+    }
+    while(getline(cin,a)){
+        if(m.count(a)){
+            cout<<m[a]<<'\n';
+        }else{
+            cout<<"eh"<<'\n';
+        }
+    }
+    return 0;
+}
