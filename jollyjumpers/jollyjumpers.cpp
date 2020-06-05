21a2e08e713672684e173ad70a158af91fdfb660
Amit Kumar <offamitkumar@gmail.com>
2020-06-05 16:22:36 +0530

    use 1d visited array

---
+++ jollyjumpers/jollyjumpers.cpp
@@ -0,0 +1,81 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+//  #include <iostream>
+//  #include <algorithm>
+//  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+  #include <cstring>
+  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e5+100;
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int n; 
+    int arr[3001];
+    int visited[(int)3e5+1000]; 
+
+    while(scanf("%d",&n)!=EOF){
+        memset(visited,-1,sizeof visited);
+        for(int i=0;i<n;++i){
+            scanf("%d",&arr[i]);
+            if(i!=0){
+                visited[abs(arr[i]-arr[i-1])]=1;
+            }
+        }
+        bool isJolly = true;
+        for(int i=1;i<n; ++i){
+            if(visited[i] == -1){
+                isJolly = false;
+                break;
+            }
+        }
+        puts(isJolly ? "Jolly" : "Not jolly");
+    }
+
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
