a863f3ad0c5219c0c9213ae329dc051c49d41d12
Amit Kumar <offamitkumar@gmail.com>
2020-06-05 17:30:03 +0530

    simple

---
+++ bela/bela.cpp
@@ -0,0 +1,81 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+//  #include <iostream>
+//  #include <algorithm>
+//  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e5+100;
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    map<char,pair<int,int>> card = {
+        {'A' ,{11,11}},
+        {'K' ,{4,4}},
+        {'Q' ,{3,3}},
+        {'J' ,{20,2}},
+        {'T' ,{10,10}},
+        {'9' ,{14,0}},
+        {'8' ,{0,0}},
+        {'7' ,{0,0}}
+    };
+    int n; char c; scanf("%d %c",&n,&c);
+    char s[5];
+    int sum{};
+    for(int i=0;i<=4*n;++i){
+        fgets(s,5,stdin);
+        if(s[1]==c){
+            sum+=card[s[0]].first;
+        }else{
+            sum+=card[s[0]].second;
+        }
+    }
+    printf("%d\n",sum);
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
