3270109b32ae4a7f6749f1b486fce7142cb855a9
Amit Kumar <offamitkumar@gmail.com>
2020-06-05 17:19:30 +0530

    simulate binary search type

---
+++ guessinggame/guessinggame.cpp
@@ -0,0 +1,85 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+  #include <iostream>
+//  #include <algorithm>
+  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e5+100;
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int n; 
+    string s; 
+    bool cheated = false;
+    int low = INT_MIN , high = INT_MAX;
+    while(scanf("%d",&n) && n){
+        getchar(); getline(cin , s);
+        if(s == "right on"){
+            if(low >= n || high <= n){
+                cheated = true;
+            }
+            puts(cheated?"Stan is dishonest" : "Stan may be honest");
+            cheated = false;
+            low = INT_MIN; high =INT_MAX;
+            continue;
+        }else if (s == "too low"){
+            if(high <= n){
+                cheated = true;
+            }
+            low = max(low , n);
+        }else if ( s == "too high" ) {
+            if(low >= n){
+                cheated = true;
+            }
+            high = min(high , n);
+        }
+    }
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
