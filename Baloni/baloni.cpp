a32fd7ae6b55713d9efdf208a184253c242af7a9
Amit Kumar <offamitkumar@gmail.com>
2020-06-06 18:16:04 +0530

    observation , to minimize brust h+1 , when encounter h

---
+++ Baloni/baloni.cpp
@@ -0,0 +1,72 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+  #include <iostream>
+//  #include <algorithm>
+//  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e6+100;
+const int MOD = (int)1e5+100;
+array<int,MAXN>mp;
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int n; scanf("%d",&n);
+    fill(mp.begin(),mp.end(),0);
+    for(int temp,i=0;i<n;++i){
+        cin >> temp; 
+        if(mp[temp]){
+            mp[temp]--;
+            mp[temp-1]++;
+        }else{
+            mp[temp-1]++;
+        }
+    }
+    cout << accumulate(mp.begin(),mp.end(),0ll) << '\n';
+
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
