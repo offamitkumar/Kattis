bccffdf79ae507d1916699babc7fb658a0fe121c
Amit Kumar <offamitkumar@gmail.com>
2020-06-12 15:34:56 +0530

    simple simulation

---
+++ transitwoes/transitwoes.cpp
@@ -0,0 +1,81 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+  #include <iostream>
+//  #include <algorithm>
+//  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e9+7;
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int s,t,n; scanf("%d %d %d",&s,&t,&n);
+    vector<int>v(n+1,-1);
+    for(auto&itr:v){cin>>itr;}
+    vector<int>bi(n,-1);
+    for(auto&itr:bi){cin >> itr;}
+    vector<int>ci(n,-1);
+    for(auto&itr:ci){cin >> itr;}
+    int Time{s};
+    for(int i=0;i<n;++i){
+        Time+=v[i];
+        int temp{ci[i]};
+        while(ci[i]+temp < Time){
+            temp+=ci[i];
+        }
+        Time = temp + bi[i];
+    }
+    Time+=v[n];
+    if(Time > t){
+        puts("no");
+    }else{
+        puts("yes");
+    }
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
