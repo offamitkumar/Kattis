2990ab3ed508db4e7c9d4a2543a4ba5f6bb31a6a
Amit Kumar <offamitkumar@gmail.com>
2020-06-12 16:04:35 +0530

    brute force will work

---
+++ welcomeeasy/welcomeeasy.cpp
@@ -0,0 +1,83 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+  #include <iostream>
+//  #include <algorithm>
+  #include <string>
+  #include <sstream>
+//  #include <fstream>
+  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e9+7;
+int total;
+const string match = "welcome to code jam";
+string s; 
+void sol(int I , int J){
+    if(J==(int)match.size()){
+        ++total;
+        return;
+    }
+    for(int i=I;i < (int)s.size();++i){
+        if(s[i] == match[J]){
+            sol(i+1,J+1);
+        }
+    }
+}
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    int t; scanf("%d",&t);
+    cin.ignore();
+    for(int i=1;i<=t;++i){
+        printf("Case #%d: ",i);
+        getline(cin , s);
+        sol(0,0);
+        total%=1000;
+        cout <<setfill('0') <<setw(4) << total << '\n';
+        total = 0;
+    }
+
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
