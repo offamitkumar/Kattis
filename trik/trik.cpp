7489c38d9bd1577dc5529f8c28bc4f32a3455d46
Amit Kumar <offamitkumar@gmail.com>
2020-06-13 14:17:26 +0530

    easy simulation

---
+++ trik/trik.cpp
@@ -0,0 +1,74 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+  #include <iostream>
+  #include <algorithm>
+  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+//  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e9+7;
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    map<char, pair<int,int>>Move;
+    Move['A'] = make_pair(1,2);
+    Move['B'] = make_pair(2,3);
+    Move['C'] = make_pair(1,3);
+    string s; cin >> s;
+    int current = 1;
+    for(auto&itr:s){
+        if(current == Move[itr].first){
+            current = Move[itr].second;
+            
+        }else if(current == Move[itr].second){
+            current = Move[itr].first;
+        }
+    }
+    cout << current << '\n'; 
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
