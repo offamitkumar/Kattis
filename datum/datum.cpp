7a9d30c9a9854869e9ea5fa3906081a2fcf7fb5c
Amit Kumar <offamitkumar@gmail.com>
2020-06-26 09:52:33 +0530

    formula based

---
+++ datum/datum.cpp
@@ -0,0 +1,154 @@
+//  Author : Amit Kumar 
+
+#include <iostream>
+#include <algorithm> 
+#include <string> 
+#include <sstream>
+#include <fstream>
+#include <iomanip> 
+#include <chrono> 
+#include <numeric> 
+#include <utility>
+#include <functional>
+
+#include <bitset>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <deque>
+#include <list>
+
+
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <cstdio>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace std::chrono;
+//  using namespace placeholders;
+using namespace __gnu_pbds;
+template<typename TypeInfo>
+using new_set = tree< 
+/* ob.order_of_key(element) -> number of element strictly less than element
+ * *ob.find_by_order(n) ->  nth element in increasing order
+ */
+         TypeInfo ,
+         null_type ,
+         less<TypeInfo> ,
+         rb_tree_tag ,
+         tree_order_statistics_node_update 
+ > ;
+
+void show_me() { cerr << endl; }
+ 
+template <typename HEAD, typename... TAIL>
+void show_me(HEAD H, TAIL... T) {
+  cerr << " " << (H);
+  show_me(T...);
+}
+ 
+#ifdef HELL_JUDGE
+#define show(...) cerr << "[" << #__VA_ARGS__ << "]:", show_me(__VA_ARGS__)
+#else
+#define show(...) 0
+#endif
+
+typedef int64_t Int;
+ 
+template<typename A, typename B>
+ostream& operator<<(ostream&out, const pair<A, B>&p){
+    out << p.first << ' ' << p.second ;
+    return out;
+}
+
+template<typename A, typename B>
+istream& operator>>(istream&in, pair<A, B>&p){
+    in >> p.first >> p.second ;
+    return in;
+}
+
+template<typename A> 
+istream& operator>>(istream&in, vector<A>&vec){
+    for(auto&itr:vec){
+        in >> itr;
+    }
+    return in;
+}
+
+template<typename A>
+ostream& operator<<(ostream&out,const vector<A>&vec){
+    for(auto&itr:vec){
+        out << itr <<' '; // VVV 
+    }
+    return out;
+}
+
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int64_t MOD  = (int64_t)1e9+7ll;
+
+int dayofweek(int d, int m, int y = 2009)  {  
+    static int t[] = { 0, 3, 2, 5, 0, 3, 
+                       5, 1, 4, 6, 2, 4 };  
+    y -= m < 3;  
+    return ( y + y / 4 - y / 100 +  
+             y / 400 + t[m - 1] + d) % 7;  
+}  
+
+signed main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    ios::sync_with_stdio(false); // FLUSH THE STREAM IF USING puts / printf / scanf/
+    cin.tie(nullptr);            //
+    cout.tie(nullptr);           //
+#ifdef HELL_JUDGE
+    auto INITIAL_TIME = high_resolution_clock::now();
+#endif 
+
+    int D,M; cin >> D >> M; 
+    switch(dayofweek(D,M)){
+        case 0: 
+            cout << "Sunday"<<'\n';
+            break;
+        case 1:
+            cout << "Monday" << '\n'; 
+            break;
+        case 2: 
+            cout << "Tuesday" <<  '\n'; 
+            break;
+        case 3:
+            cout << "Wednesday" << '\n'; 
+            break;
+        case 4: 
+            cout << "Thursday" << '\n'; 
+            break;
+        case 5:
+            cout << "Friday" << '\n'; 
+            break;
+        case 6:
+            cout << "Saturday" << '\n'; 
+            break;
+    }
+
+#ifdef HELL_JUDGE
+    auto FINAL_TIME = high_resolution_clock::now();
+    cout << "Time : "
+         << duration_cast<milliseconds>(FINAL_TIME-INITIAL_TIME).count() 
+         << " ms" << '\n'; 
+#endif 
+    return 0;
+}
